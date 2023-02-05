ea1bbdb77d360efdda98eb3f55632e1e10e58dea
TensegrityBlockxels <103687791+TensegrityBlockxels@users.noreply.github.com>
2023-02-05 10:22:02 -0300

    New gh-pages branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# My blog
-(using [sveltekit](https://kit.svelte.dev))
--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import style from '$styles/style.css'
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="/blog/">my blog</a>.</p>
