53712535094aab2ae89066186fc3b149fb84e48f
TensegrityBlockxels <103687791+TensegrityBlockxels@users.noreply.github.com>
2024-01-11 16:44:03 -0300

    Added project list.

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addFilter("excerpt", (post) => {
 		const content = post.replace(/(<([^>]+)>)/gi, "");
-		return content.substr(0, content.lastIndexOf(" ", 200)) + "...";
+		return content
 	});
 
 	eleventyConfig.addPassthroughCopy("src/styles/*.css");
